777aaef00de03acdd217a39be71006c72cd1cb52
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-03-29 11:32:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner a = new Scanner(System.in);
+    double x=a.nextDouble();
+    if(x%4==0)
+    {
+      System.out.println("rok przestępny");
+    }
+    else
+    {
+      System.out.println("rok nieprzestępny");
+    }
+
+  }
+}
